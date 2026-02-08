636c61f047a44fa650199aba3c47439dc13635cb
ashirwadexe <ashirwadbappy@gmail.com>
2026-02-08 15:04:48 +0530

    adding teams section to the codebase

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WorkCard = ({work, index}) => {
     
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col hover:scale-103 transition-all duration-500'>
         <img className='w-full rounded-xl' src={work.icon} alt="" />
         <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
         <p className='text-sm opacity-60 w-5/6'>{work.desc}</p>
