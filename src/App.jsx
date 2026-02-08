2f7a6f8b28d10ddc42fc49c7deb8fe4a32fef563
ashirwadexe <ashirwadbappy@gmail.com>
2026-02-08 13:59:38 +0530

    adding services sections to the codebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
+import Services from './components/Services';
 
 const App = () => {
   // here we are getting the theme from local storage 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme}/>
       <Hero/>
       <TrustedBy/>
+      <Services/>
     </div>
   )
 }
